1dbe11c0ce532df433b2575dc3543dda993a20d7
Sait Çakır <sait25@windowslive.com>
2022-08-29 14:54:17 +0300

    crud işlemleri için generic öncesi interfaceler ve repositoryler oluşturuldu. Ayrıca contextte ki blog tablosu eksikliği giderildi.

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccessLayer.Abstract
+{
+    internal interface IBlogDal
+    {
+        List<Blog> ListAllBlog();
+        void AddBlog(Blog blog);
+        void DeleteBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetBlog(int id);
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -16,6 +16,7 @@ namespace DataAccessLayer.Concrete
         }
 
         public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Contact> Contacts { get; set; }
