079e4ad5c544f3eaf1a525cadddcce750ff9dd45
Sait Çakır <sait25@windowslive.com>
2022-09-02 08:21:00 +0300

    Blog sayfasına veri çekme işlemi yapıldı.

---
+++ DataAccessLayer/Migrations/20220901124855_mig5.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BlogThumbnailImage",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlogImage",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "BlogThumbnailImage",
+                table: "Blogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "BlogImage",
+                table: "Blogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- EntityLayer/Concrete/Blog.cs
+++ EntityLayer/Concrete/Blog.cs
@@ -13,8 +13,8 @@ namespace EntityLayer.Concrete
         public int BlogID { get; set; }
         public string BlogTitle { get; set; }
         public string BlogContent { get; set; }
-        public bool BlogThumbnailImage { get; set; }
-        public bool BlogImage { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
         public string BlogCreateDate { get; set; }
         public bool BlogStatus { get; set; }
         public int CategoryID { get; set; }
